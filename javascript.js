e85f2dae8b182e69c69412c4346ffffa8ac62223
SreetilakAjith <150450099+SreetilakAjith@users.noreply.github.com>
2024-01-27 21:54:28 +0300

    Update javascript.js
    Added AES Encryption

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 let loginButton = document.getElementById("loginButton");
 let username = document.getElementById("usernameInput");
 let password = document.getElementById("passwordInput");
@@ -7,20 +8,23 @@ let maxTry = 3;
 
 window.SuccessfulUsername = "";
 
+const Cypher = "6544dsf4sdf4sd4fwe4rg6tg4w5fv4t8gw48g4r";
 
 //Data
-let Usernames = ["a2FzaQ==", "a2FybmFu", "c2hlZWJh", "YWRtaW4="];
-let Passwords = ["a2FzaUA=", "a2FybmFuQA==", "c2hlZWJhQA==", "cGFzc3dvcmQ="];
+let Usernames = ["U2FsdGVkX1/A/NwgPfsDnY2APImSA6PtUd/xKWlz87I=", "U2FsdGVkX1/H15aR6MFaSureVerbaFrnHbCZsdBpZW8=", "U2FsdGVkX1+eUIUI7R1xim/vNto2vBA7fwKuxL/E/Ac="];
+let Passwords = ["U2FsdGVkX199Ni8rdErVPEXeCtwDQL4G+r5uGYU3RZA=", "U2FsdGVkX1+VC6gyBwN//GWPw+YNaGxfpessXC9J6jo=", "U2FsdGVkX18Lt0qyaDCg10w/s5iszYoe1LyYN1lN9ak="];
 
 
 function GetDetails(){
     login = false;
 
+    // Identification with Details
+
     for(let i = 0; i < Usernames.length; i++){
-        if(username.value == atob(Usernames[i]) && password.value == atob(Passwords[i])){
+        if(username.value == CryptoJS.AES.decrypt(Usernames[i], Cypher).toString(CryptoJS.enc.Utf8) && password.value == CryptoJS.AES.decrypt(Passwords[i], Cypher).toString(CryptoJS.enc.Utf8)){
 
             username.value = "Successful !";
-            SuccessfulUsername = atob(Usernames[i]);
+            SuccessfulUsername = Usernames[i];
             login = true;
             username.style.borderColor = "greenyellow";
             password.style.borderColor = "greenyellow";
@@ -34,6 +38,8 @@ function GetDetails(){
         }
     }
 
+    // Check Login Status
+
     if(login == false){
         username.value = "Username / Password Incorrect !";
         alert("Inavlid Username / Password");
@@ -44,10 +50,13 @@ function GetDetails(){
         console.log("Try: " + userTry);
     }
 
+    // Check Attemtps
+
     if(userTry >= maxTry){
         username.value = "Forgot Password / Username?";
         alert("Contact - 123*9*9*9*");
         loginButton.disabled = true;
+        loginButton.textContent = "Disabled";
     }
 
 
